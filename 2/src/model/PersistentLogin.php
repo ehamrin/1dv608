cbba09b2141c4504922ca477711563f8a993220a
ehamrin <eh222ve@student.lnu.se>
2015-09-02 22:09:39 +0200

    Added days valid variable

--- 2/src/model/PersistentLogin.php
+++ 2/src/model/PersistentLogin.php
@@ -10,10 +10,12 @@ class PersistentLogin
     public $securityString;
     public $expiration;
 
+    private static $days = 10;
+
     private static $salt = "1dv608";
 
     public function __construct($user){
-        $this->expiration = time() + 24*3600;
+        $this->expiration = time() + 24*3600* self::$days;
         $this->securityString = sha1(self::$salt . uniqid() . self::$salt);
         $this->user = $user;
 
