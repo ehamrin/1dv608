8e5eaeb72978fa4727c0284172af71cd3ac447d0
ehamrin <eh222ve@student.lnu.se>
2015-10-12 17:50:13 +0200

    Added some RegEx constants to class

--- Project/index.php
+++ Project/index.php
@@ -4,48 +4,58 @@ require_once 'src/Form/controller/FormController.php';
 
 $form = new \Form\controller\FormController("RegistrationForm");
 
+use \Form\model\input;
+use \Form\model\validation;
+use \Form\model\comparator;
+
 $form->AddInput(
-    ((new \Form\model\input\Text("RegistrationView::Username"))
+    ((new input\Text("RegistrationView::Username"))
         ->SetLabel("Username")
         ->SetValidation(
-            new \Form\model\validation\Required("You must enter a username"),
-            new \Form\model\validation\MinLength(3, "Username must be longer than 3 characters")
+            new validation\Required("You must enter a username"),
+            new validation\MinLength(3, "Username must be longer than 3 characters")
         )
     ),
-    ((new \Form\model\input\Text("RegistrationView::Firstname"))
+    ((new input\Text("RegistrationView::Firstname"))
         ->SetLabel("First name")
         ->SetValidation(
-            new \Form\model\validation\MaxLength(25, "First name must be shorter than 25 characters")
+            new validation\MaxLength(25, "First name must be shorter than 25 characters")
         )
     ),
-    ((new \Form\model\input\Text("RegistrationView::LastName"))
+    ((new input\Text("RegistrationView::LastName"))
         ->SetLabel("Last name")
         ->SetValidation(
-            new \Form\model\validation\MaxLength(25, "Last name must be shorter than 25 characters")
+            new validation\MaxLength(25, "Last name must be shorter than 25 characters")
         )
     ),
-    ((new \Form\model\input\Text("RegistrationView::PersonalIdentification"))
+    ((new input\Text("RegistrationView::PersonalIdentification"))
         ->SetLabel("Personal ID")
         ->SetValidation(
-            new \Form\model\validation\RegEx("/^(19|20)?[0-9]{6}(-)?[0-9pPtTfF][0-9]{3}$/", "Personal ID must match YYYYMMDD-XXXX")
+            new validation\RegEx("/^(19|20)?[0-9]{6}(-)?[0-9pPtTfF][0-9]{3}$/", "Personal ID must match YYYYMMDD-XXXX")
         )
     ),
-    ((new \Form\model\input\Password("RegistrationView::Password"))
+    ((new input\Text("RegistrationView::PostalCode"))
+        ->SetLabel("Postal Code")
+        ->SetValidation(
+            new validation\RegEx(validation\RegEx::SWEDISH_POSTAL_CODE, "Personal ID must match YYYYMMDD-XXXX")
+        )
+    ),
+    ((new input\Password("RegistrationView::Password"))
         ->SetLabel("Password")
         ->SetValidation(
-            new \Form\model\validation\Required("You must fill in a password"),
-            new \Form\model\validation\MinLength(6, "Password must be longer than 6 characters")
+            new validation\Required("You must fill in a password"),
+            new validation\MinLength(6, "Password must be longer than 6 characters")
         )
     ),
-    ((new \Form\model\input\Password("RegistrationView::PasswordRepeat"))
+    ((new input\Password("RegistrationView::PasswordRepeat"))
         ->SetLabel("Repeat Password")
         ->SetValidation(
-            new \Form\model\validation\Required("You must fill in a password"),
-            new \Form\model\validation\MinLength(6, "Password must be longer than 6 characters")
+            new validation\Required("You must fill in a password"),
+            new validation\MinLength(6, "Password must be longer than 6 characters")
         )
-        ->SetComparator(new \Form\model\comparator\EqualTo("RegistrationView::Password", "Must match Password"))
+        ->SetComparator(new comparator\EqualTo("RegistrationView::Password", "Must match Password"))
     ),
-    (new \Form\model\input\Submit("RegistrationView::Register", "Register"))
+    (new input\Submit("RegistrationView::Register", "Register"))
 );
 
 $message = "";
