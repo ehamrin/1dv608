94530c3d49267b63cd8796dd6dcb7eae70e20a09
ehamrin <eh222ve@student.lnu.se>
2015-10-12 19:43:21 +0200

    Added Checkbox, Radio, Textarea and fixed "selected" error in Select Element

--- Project/src/Form/view/InputHTML/Select.php
+++ Project/src/Form/view/InputHTML/Select.php
@@ -14,7 +14,7 @@ if(!isset($input)){
     <label for="<?php echo $input->GetName(); ?>"><?php echo $input->GetLabel(); ?></label>
     <select name="<?php echo $input->GetName(); ?>" id="<?php echo $input->GetName(); ?>">
 <?php foreach($input->GetOptions() as $option): ?>
-        <option value="<?php echo $option->value; ?>" <?php echo $option->name == $input->GetValue() ? 'selected' : ''; ?>><?php echo $option->name; ?></option>
+        <option value="<?php echo $option->value; ?>" <?php echo $option->value == $input->GetValue() ? 'selected' : ''; ?>><?php echo $option->name; ?></option>
 <?php endforeach; ?>
     </select>
     <?php echo $errormessages; ?>
